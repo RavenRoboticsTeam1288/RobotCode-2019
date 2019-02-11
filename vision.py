114600e067f40d6f70c11c2b048509fa8e93484c
Stephen Derenski <33239267+Scoutsniper674@users.noreply.github.com>
2019-02-10 19:59:44 -0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,4 +1,4 @@
-from cscore import *
+from cscore import CameraServer
 
 def main():
     cs = CameraServer.getInstance()
@@ -7,10 +7,10 @@ def main():
 
     #kKeepOpen = cs.VideoSource.ConnectionStrategy.kConnectionKeepOpen
     
-    #usb1 = cs.startAutomaticCapture(dev=0)
-    #usb2 = cs.startAutomaticCapture(dev=1)
+    usb1 = cs.startAutomaticCapture(dev=0)
+    usb2 = cs.startAutomaticCapture(dev=1)
 
-    cs.addServer("Server 1", 1181)
+    #cs.addServer("Server 1", 1181)
 
     #usb1.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
     #usb2.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
