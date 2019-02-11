a3aafb0fef63a5cd515e68371035ad511749fa6c
m-tiller <47511189+m-tiller@users.noreply.github.com>
2019-02-10 19:40:47 -0600

    Seperate CameraServers
    Separated into two cameraServers and defined specific ports for the web mpeg stream allowing for two tabs and one camera on each window.  For real though, don't make those variables global.... you monster.

--- vision.py
+++ vision.py
@@ -1,18 +1,24 @@
 from cscore import CameraServer
 
-def main():
-    cs = CameraServer.getInstance()
-    #server = cs.getServer()
-    cs.enableLogging()
+### Oh my god, don't make it a global variable ###
+#camSrv1 = CameraServer.addServer(name="camera1",port=1181)
+#camSrv2 = CameraServer.addServer(name="camera2",port=1182)
+### Seriously, please, don't f***ing do it ###
 
-    #kKeepOpen = cs.VideoSource.ConnectionStrategy.kConnectionKeepOpen
-    
-    usb1 = cs.startAutomaticCapture(name="cam1", path='/dev/v4l/by-id/some-path-here')
-    usb2 = cs.startAutomaticCapture(name="cam2", path='/dev/v4l/by-id/some-other-path-here')
+def camera1():
+    camSrv1 = CameraServer.addServer(name="camera1",port=1181)
+    #if you decided to be an a**hole and make it global, comment the line above and uncomment the line below#
+    #instance1 = camSrv1.getInstance()
+    camSrv1.enableLogging()
+    usb1Src = camSrv1.startAutomaticCapture(name="cam1", path='/dev/v4l/by-id/some-path-here')
+    showTime1 = camSrv1.getVideo()
+    camSrv1.waitForever()
 
-    #cs.addServer("Server 1", 1181)
-
-    #usb1.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
-    #usb2.setConnectionStrategy(VideoSource.ConnectionStrategy.kKeepOpen)
-
-    cs.waitForever()
+def camera2():
+    camSrv2 = CameraServer.addServer(name="camera2",port=1182)
+    #if you decided to be an a**hole and make it global, comment the line above and uncomment the line below#
+    #instance2 = camSrv2.getInstance()
+    camSrv2.enableLogging()
+    usb2Src = camSrv2.startAutomaticCapture(name="cam2", path='/dev/v4l/by-id/some-other-path-here')
+    showTime2 = camSrv2.getVideo()
+    camSrv2.waitForever()
