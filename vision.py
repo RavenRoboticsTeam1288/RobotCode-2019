efcc8c70768611023fd3adf0a315d8bba4102792
Stephen Derenski <33239267+Scoutsniper674@users.noreply.github.com>
2019-02-12 20:14:00 -0600

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,24 +1,11 @@
 from cscore import CameraServer
 
-### Oh my god, don't make it a global variable ###
-#camSrv1 = CameraServer.addServer(name="camera1",port=1181)
-#camSrv2 = CameraServer.addServer(name="camera2",port=1182)
-### Seriously, please, don't f***ing do it ###
+def main():
+    cs = CameraServer.getInstance()
+    cs.enableLogging()
 
-def camera1():
-    camSrv1 = CameraServer.addServer(name="camera1",port=1181)
-    #if you decided to be an a**hole and make it global, comment the line above and uncomment the line below#
-    #instance1 = camSrv1.getInstance()
-    camSrv1.enableLogging()
-    usb1Src = camSrv1.startAutomaticCapture(name="cam1", path='/dev/v4l/by-id/some-path-here')
-    showTime1 = camSrv1.getVideo()
-    camSrv1.waitForever()
+    usb1 = cs.startAutomaticCapture(name='cam1', path='/dev/v4l/by-path/platform-ci_hdrc.0-usb-0:1.1:1.0-video-index0')
 
-def camera2():
-    camSrv2 = CameraServer.addServer(name="camera2",port=1182)
-    #if you decided to be an a**hole and make it global, comment the line above and uncomment the line below#
-    #instance2 = camSrv2.getInstance()
-    camSrv2.enableLogging()
-    usb2Src = camSrv2.startAutomaticCapture(name="cam2", path='/dev/v4l/by-id/some-other-path-here')
-    showTime2 = camSrv2.getVideo()
-    camSrv2.waitForever()
+    usb2 = cs.startAutomaticCapture(name='cam2', path='/dev/v4l/by-path/platform-ci_hdrc.0-usb-0:1.2:1.0-video-index0')
+
+    cs.waitForever()
