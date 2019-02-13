015b9220c2c0beccec2d05207639a4a1376852c7
Stephen Derenski <33239267+Scoutsniper674@users.noreply.github.com>
2019-02-12 20:14:16 -0600

    Update vision.py

--- vision.py
+++ vision.py
@@ -5,7 +5,6 @@ def main():
     cs.enableLogging()
 
     usb1 = cs.startAutomaticCapture(name='cam1', path='/dev/v4l/by-path/platform-ci_hdrc.0-usb-0:1.1:1.0-video-index0')
-
     usb2 = cs.startAutomaticCapture(name='cam2', path='/dev/v4l/by-path/platform-ci_hdrc.0-usb-0:1.2:1.0-video-index0')
 
     cs.waitForever()
